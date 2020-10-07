c4de7f6baeb0bbaabac8a623c9a1de43f3a1bbc8
andres-paul <72525298+andres-paul@users.noreply.github.com>
2020-10-07 16:54:27 -0500

    Agregué mi nombre con printf

--- HolaMundo2.c
+++ HolaMundo2.c
@@ -4,5 +4,6 @@ printf("\nFernando Julian Diaz Vazquez\n");
 printf("Este es mi segundo codigo");
 printf("Programar es facil");
 printf("Voy a compartir con mi compañero el codigo");
+printf("n\Andres Paul\n");
 return 0;
 }
